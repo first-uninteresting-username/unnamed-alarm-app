6a5b66868ae962a8b502e3ca86c19e4d26ec67b4
First-Non-Interesting-Username <janekmusin@proton.me>
2026-07-27 12:28:31 +0200

    chore: scaffold android app structure

---
+++ code/app/src/main/java/io/github/first_non_interesting_username/unnamedalarmapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package io.github.first_non_interesting_username.unnamedalarmapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.first_non_interesting_username.unnamedalarmapp.ui.theme.UnnamedAlarmAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnnamedAlarmAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    UnnamedAlarmAppTheme {
+        Greeting("Android")
+    }
+}
